196784537220502c52f569e944bb33a52f10d713
a <a@as-iMac.local>
2016-01-01 01:43:42 +0200

    docs 2

---
+++ search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['bluetoothdevice',['BluetoothDevice',['../class_tech_tweaking_1_1_bluetooth_1_1_bluetooth_device.html',1,'TechTweaking::Bluetooth']]],
+  ['bt',['Bt',['../class_tech_tweaking_1_1_bluetooth_1_1_bt.html',1,'TechTweaking::Bluetooth']]]
+];
